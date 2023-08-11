1b6037a6e0ac0c7e78d1275e4d8f37517b6141c4
arturkowalik <96322990+arturkowalik@users.noreply.github.com>
2023-08-11 11:56:57 +0200

    heli controller
    se 5 e 25

---
+++ Assets/Intro_Heli_Physics/Code/Scripts/Input/IP_KeyboardHeli_Input.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IndiePixel
+{
+	public class IP_KeyboardHeli_Input : IP_BaseHeli_Input
+	{
+		#region variables
+		[Header("Heli Keyboard Inputs")]
+		#endregion
+		
+		#region Properties
+		private float throttleInput = 0f;
+		public float ThrottleInput
+		{
+			get { return throttleInput; }
+		}
+		
+		private float collectiveInput = 0f;
+		public float CollectiveInput
+		{
+			get { return collectiveInput; }
+		}
+		
+		private Vector2 cyclicInput = Vector2.zero;
+		public Vector2 CyclicInput
+		{
+			get { return cyclicInput; }
+		}
+		
+		private float pedalInput = 0f;
+		public float PedalInput
+		{
+			get { return pedalInput; }
+		}
+		
+		
+		#endregion
+		
+		#region BuiltIn Methods
+	    #endregion
+	    
+	    #region Custom Methods
+		protected override void HandleInputs()
+		{
+			base.HandleInputs();
+			
+			HandleThrottle();
+			HandleCollective();
+			HandleCyclic();
+			HandlePedal();
+		}
+		
+		void HandleThrottle()
+		{
+
+		}
+		
+		void HandleCollective()
+		{
+
+		}	
+				
+		void HandleCyclic()
+		{
+			cyclicInput.y = vertical;
+			cyclicInput.x = horizontal;
+		}
+		
+		void HandlePedal()
+		{
+
+		}		
+		
+	
+
+	    #endregion
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
