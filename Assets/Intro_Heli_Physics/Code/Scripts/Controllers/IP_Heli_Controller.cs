68bbca4b773c56b70453ea5273d164ed9c49bcac
arturkowalik <96322990+arturkowalik@users.noreply.github.com>
2023-08-12 23:13:43 +0200

    Set up menu

---
+++ Assets/Intro_Heli_Physics/Code/Scripts/Controllers/IP_Heli_Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IndiePixel
+{
+	[RequireComponent(typeof(IP_Input_Controller), typeof(IP_KeyboardHeli_Input), typeof(IP_XboxHeli_Input))]	
+	public class IP_Heli_Controller : IP_Base_RBController
+	{
+		#region Variables
+		//[Header("Controller Properties")]
+		private IP_Input_Controller input;
+		#endregion
+	
+		void Start()
+		{
+			input = GetComponent<IP_Input_Controller>();
+		}
+	
+		#region Custom Methods	
+		protected override void HandlePhysics()
+		{
+			
+			if	(input)
+			{
+				HandleEngines();
+				HandleCharacteristics();
+			}
+		}	
+		#endregion
+	
+	
+	
+		#region Helicopter Controler Methods
+		protected virtual void HandleEngines()
+		{
+			
+		}
+		
+		protected virtual void HandleCharacteristics()
+		{
+			
+		}
+		#endregion
+
+	}
+}
--- Assets/Intro_Heli_Physics/Code/Scripts/Input/IP_Input_Controller.cs
+++ Assets/Intro_Heli_Physics/Code/Scripts/Input/IP_Input_Controller.cs
@@ -12,42 +12,48 @@ namespace IndiePixel
 		Mobile,
 	}
 	
+	[RequireComponent(typeof(IP_KeyboardHeli_Input), typeof(IP_XboxHeli_Input))]
 	public class IP_Input_Controller : MonoBehaviour
 	{
 		#region Variables
+		[Header("Input Properties")]
 		public InputType inputType = InputType.Keyboard;
 		
-		[Header("Input Components")]
-		public IP_KeyboardHeli_Input keyInput;
-		public IP_XboxHeli_Input xboxInput;
+		private IP_KeyboardHeli_Input keyInput;
+		private IP_XboxHeli_Input xboxInput;
 		#endregion
+		
 		
 		#region BuiltIn Methods
 	    void Start()
-	    {
-		    SetInputType(inputType);
+		{
+			keyInput = GetComponent<IP_KeyboardHeli_Input>();
+			xboxInput = GetComponent<IP_XboxHeli_Input>(); 
+			
+			if(keyInput && xboxInput)
+			{
+				SetInputType(inputType);
+			}
 	    }
 		#endregion
 		
 		
+		 
+		 
 		#region Custom Methods
 		void SetInputType(InputType type)
 		{
-			if(keyInput && xboxInput)
+			if(type == InputType.Keyboard)
 			{
-				if(type == InputType.Keyboard)
-				{
-					keyInput.enabled = true;
-					xboxInput.enabled = false;
-				}
-				
-				if(type == InputType.Xbox)
-				{
-					xboxInput.enabled = true;
-					keyInput.enabled = false;
-				}
+				keyInput.enabled = true;
+				xboxInput.enabled = false;
 			}
-
+			
+			if(type == InputType.Xbox)
+			{
+				xboxInput.enabled = true;
+				keyInput.enabled = false;
+			} 
 		}
 		
 		#endregion
